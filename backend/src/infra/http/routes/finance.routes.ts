3c6bf2d2f2796385c61c0973eb2e4b48420cb69d
Jackson Santos <jacksyntax20@gmail.com>
2025-11-26 21:39:08 -0300

    docs(finance): add swagger documentation for interest calculation endpoint

--- backend/src/infra/http/routes/finance.routes.ts
+++ backend/src/infra/http/routes/finance.routes.ts
@@ -9,6 +9,26 @@ export default function financeRoutes(app: FastifyInstance) {
 	app.post(
 		'/calcular-juros',
 		{
+			schema: {
+				tags: ['Financeiro'],
+				summary: 'Calcula o juros simples',
+				description:
+					'Calcula o juros simples com base na taxa, valor e tempo(data de vencimento) fornecidos.',
+				body: {
+					type: 'object',
+					properties: {
+						valor: {
+							type: 'number',
+							description: 'Valor principal',
+						},
+						dataVencimento: {
+							type: 'string',
+							description: 'Data de vencimento no formato ISO 8601',
+						},
+					},
+					required: ['valor', 'dataVencimento'],
+				},
+			},
 			preHandler: validateBodyMiddleware(calcularJurosSchema),
 		},
 		async (req, res) => controller.handle(req, res),
