a200556fa15f97e7698a56683070a730d93f3561
Jackson Santos <jacksyntax20@gmail.com>
2025-11-26 21:36:48 -0300

    docs(vendedores): add swagger documentation for sales routes

--- backend/src/infra/http/routes/vendedores.routes.ts
+++ backend/src/infra/http/routes/vendedores.routes.ts
@@ -4,6 +4,27 @@ import { makeVendedoresController } from '../../factories/make-vendedores-contro
 export function vendedoresRoutes(app: FastifyInstance) {
 	const controller = makeVendedoresController()
 
-	app.get('/', async (req, res) => controller.index(req, res))
-	app.get('/comissao', async (req, res) => controller.getComissoes(req, res))
+	app.get(
+		'/',
+		{
+			schema: {
+				tags: ['Vendas'],
+				summary: 'Retorna a lista de vendas por vendedor',
+				description: 'Retorna a lista de vendas por vendedor cadastrados',
+			},
+		},
+		async (req, res) => controller.index(req, res),
+	)
+	app.get(
+		'/comissao',
+		{
+			schema: {
+				tags: ['Vendas'],
+				summary: 'Retorna as vendas e comissões por vendedor',
+				description:
+					'Retorna as vendas, comissões por vendedor e resumo das comissões por vendedor.',
+			},
+		},
+		async (req, res) => controller.getComissoes(req, res),
+	)
 }
